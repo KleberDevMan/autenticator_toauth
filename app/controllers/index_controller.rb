c8fa312f8ce1dc1e5a12e981764ad20dff5aaf30
KleberDevMen <klebersubcontas@gmail.com>
2018-12-21 11:33:51 -0300

    Perguntas e Respostas aleatorias. 'Qual o seu primeiro nome?' e 'Em que bairro vc vive?'

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
 
-  before_action :set_questions_type, only: %i[sign_up]
+  before_action :init, only: %i[sign_up]
 
   # ---- VIEW ----
   def index
@@ -8,6 +8,8 @@ class IndexController < ApplicationController
 
   def sign_up
     if params[:cpf] === '69199128055'
+
+      # busca cpf na Receita Federal
 
       # Crio a requisicao do usuario e salvo no banco
       $user_request = UserRequest.create(cpf: "69199128055", value: false, json_result: "Aqui sera armazenado o .json vindo da receita")
@@ -18,50 +20,65 @@ class IndexController < ApplicationController
 
   def questions
     # Crio a primeira pergunta
-    generate_question()
+    if $request_question == nil
+      generate_question()
+      puts $answers.inspect
+      puts $request_question.inspect
+    end
   end
 
   # --- UTIL ----
   def generate_question
-
-    #Busco uma Questao aleatoria no banco
+    # Busco uma Questao aleatoria no banco
     question = Question.all.sample
-
     # Crio pergunta baseado na requisicao do usuario
-    request_question = RequestQuestion.new(desc: question.desc, question: question, user_request: $user_request)
-
-    # Gero 3 respostas aleatórias, baseadas na questao que veio
-    answers = generate_anwser(request_question, 3)
-
-
-    puts "kleber junio"
-    # puts question.inspect
-    # puts $request_question.inspect
-    puts answers.inspect
+    $request_question = RequestQuestion.new(desc: question.desc, question: question, user_request: $user_request)
+    # Gero 3 respostas aleatórias, baseadas na questao aleatoria buscada
+    $answers = generate_anwsers($request_question, 3)
+  end
+  def result
+    result = false
   end
 
-  #
-  def generate_anwser(type, questions_nubers)
-    # respostas
-    anwers = []
-
-    id = type.question.question_type.id
-
-    # Se quer respostas do tipo: Nome
-    if id === $name_type
-      # Adiciona alterantivas falsas do tipo da pergunta
-      for i in 1..questions_nubers
-        anwers << Answer.find(2)
-      end
-    end
-
-    return anwers
-  end
 
   private
+  def init
+    # Variaveis globais
+    $request_question = nil
+    $answers = []
 
-  def set_questions_type
-    $name_type = QuestionType.find(1).id
+    # Tipos de questoes
+    $name_type = QuestionType.find(1)
+    $district_type = QuestionType.find(2)
+  end
+  def generate_anwsers(request_question, questions_nubers)
+
+    anwers = []
+
+    question_type_id = request_question.question.question_type.id
+
+    # Se quer respostas do tipo: Nome
+    if question_type_id === $name_type.id
+      # Popula lista de respostas falsas
+      for i in 1..questions_nubers
+        anwer = Answer.new(desc: Name.all.sample.desc, value: false, selected: false, request_question: request_question)
+        anwers << anwer
+      end
+
+      # Retorna lista de respostas falsas
+      return anwers
+    end
+
+    # Se quer respostas do tipo: Bairro
+    if question_type_id === $district_type.id
+      for i in 1..questions_nubers
+        anwer = Answer.new(desc: District.all.sample.desc, value: false, selected: false, request_question: request_question)
+        anwers << anwer
+      end
+
+      # Retorna list de respostas falsas
+      return anwers
+    end
   end
 
 end
