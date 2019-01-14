3f7568035d4543cf76eafb7ad5ee809c6c7f65d7
KleberDevMen <klebersubcontas@gmail.com>
2019-01-14 16:52:56 -0300

    Uso de array para as questions

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -24,16 +24,11 @@ class IndexController < ApplicationController
 
         user_request = UserRequest.create(cpf: cpf, value: false, json_result: 'Dado do Json da Receita Federal', return_web_service: true, jsonb_result: user)
 
-        # $q1 = user_request.request_questions[0]
-        # $q2 = user_request.request_questions[1]
-        # $q3 = user_request.request_questions[2]
-
         $questions = Array.new
         $questions = user_request.request_questions
-
       end
     rescue => error
-      puts error.backtrace
+      flash[:notice] = error.message
       redirect_to index_path
     end
 
@@ -41,12 +36,6 @@ class IndexController < ApplicationController
 
   def result
     $issues_submitted = nil
-
-    if params[:q1] != nil || params[:q1] != nil || params[:q1] != nil
-      $q1.value = params[:q1]
-      $q2.value = params[:q2]
-      $q3.value = params[:q3]
-    end
   end
 
 end
